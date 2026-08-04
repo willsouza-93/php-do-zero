73526eaf39d1049202e8ca0cf93b792bcdc9431f
willsouza-93 <willianmsouza.93@gmail.com>
2026-08-03 22:55:43 -0300

    feat: adiciona enum da aula 9

---
+++ aula-09/TransactionType.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+enum TransactionType: string
+{
+    case Income = 'income';
+    case Expense = 'expense';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Income => 'Receita',
+            self::Expense => 'Despesa',
+        };
+    }
+}
